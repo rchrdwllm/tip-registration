472535aa04bed046c2c4bafac551cbf5a6f2abca
Richard William <rwilliam.flores@gmail.com>
2023-10-07 15:17:38 +0800

    made reusable functions for page transitions

--- js/script.js
+++ js/script.js
@@ -27,18 +27,26 @@ barba.init({
             beforeEnter(data) {
                 updateLink(data.next.namespace);
             },
+            afterLeave(data) {
+                wrapInputs(data.next.container);
+                addInputValidations(data.next.container);
+            },
         },
         {
             namespace: 'education',
             beforeEnter(data) {
                 updateLink(data.next.namespace);
             },
+            afterLeave(data) {
+                wrapInputs(data.next.container);
+                addInputValidations(data.next.container);
+            },
         },
     ],
 });
 
 function updateLink(namespace) {
-    navLinks.forEach((link) => {
+    navLinks.forEach(link => {
         const linkNamespace = link.dataset.namespace;
 
         if (linkNamespace === namespace) {
@@ -49,72 +57,78 @@ function updateLink(namespace) {
     });
 }
 
-document.querySelectorAll('input').forEach((el) => {
-    const clone = el.cloneNode();
-    const wrapper = document.createElement('div');
+function wrapInputs(container) {
+    container.querySelectorAll('input').forEach(el => {
+        const clone = el.cloneNode();
+        const wrapper = document.createElement('div');
 
-    clone.classList.add('input');
+        clone.classList.add('input');
 
-    wrapper.classList.add('input-container');
-    wrapper.appendChild(clone);
+        wrapper.classList.add('input-container');
+        wrapper.appendChild(clone);
 
-    el.parentElement.replaceChild(wrapper, el);
-});
-document.querySelectorAll('input').forEach((el) => {
-    const dataset = el.dataset;
-    const keys = Object.keys(dataset);
+        el.parentElement.replaceChild(wrapper, el);
+    });
+}
 
-    const regex = /(?:regex|hint)-(\d)/;
-    const indices = keys
-        .map((v) => {
-            const match = regex.exec(v);
-            if (match == null) {
-                return null;
+function addInputValidations(container) {
+    container.querySelectorAll('input').forEach(el => {
+        const dataset = el.dataset;
+        const keys = Object.keys(dataset);
+
+        const regex = /(?:regex|hint)-(\d)/;
+        const indices = keys
+            .map(v => {
+                const match = regex.exec(v);
+                if (match == null) {
+                    return null;
+                }
+
+                return parseInt(match[1]);
+            })
+            .reduce((a, b) => {
+                a.add(b);
+
+                return a;
+            }, new Set());
+
+        let previous = el.value;
+        el.addEventListener('focus', () => {
+            previous = el.value;
+        });
+        el.addEventListener('blur', () => {
+            /// Assuming that the events go like focus -> blur,
+            /// we can check if the value has changed.
+
+            if (previous === el.value) {
+                return;
             }
 
-            return parseInt(match[1]);
-        })
-        .reduce((a, b) => {
-            a.add(b);
+            /// If the data *has* changed, then we should validate.
 
-            return a;
-        }, new Set());
+            /// Flag that indicates whether any validation has failed.
+            let hasFailed = false;
+            for (const index of indices) {
+                const regexp = new RegExp(el.dataset[`regex-${index}`], 'gi');
+                const hint = el.dataset[`hint-${index}`];
 
-    let previous = el.value;
-    el.addEventListener('focus', () => {
-        previous = el.value;
-    });
-    el.addEventListener('blur', () => {
-        /// Assuming that the events go like focus -> blur,
-        /// we can check if the value has changed.
+                const succeeds = regexp.test(el.value);
+                if (!succeeds) {
+                    el.parentElement.setAttribute('active-hint', hint);
+                    el.parentElement.classList.add('error');
+                    hasFailed = true;
 
-        if (previous === el.value) {
-            return;
-        }
-
-        /// If the data *has* changed, then we should validate.
-
-        /// Flag that indicates whether any validation has failed.
-        let hasFailed = false;
-        for (const index of indices) {
-            const regexp = new RegExp(el.dataset[`regex-${index}`], 'gi');
-            const hint = el.dataset[`hint-${index}`];
-
-            const succeeds = regexp.test(el.value);
-            if (!succeeds) {
-                el.parentElement.setAttribute('active-hint', hint);
-                el.parentElement.classList.add('error');
-                hasFailed = true;
-
-                // no foreach :P
-                break;
+                    // no foreach :P
+                    break;
+                }
             }
-        }
 
-        if (!hasFailed) {
-            el.parentElement.classList.remove('error');
-        }
-
-        console.log(el.parentElement);
+            if (!hasFailed) {
+                el.parentElement.classList.remove('error');
+            }
+        });
     });
-});
+}
+
+wrapInputs(document);
+addInputValidations(document);
